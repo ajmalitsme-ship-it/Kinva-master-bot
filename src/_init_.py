07d64948b004c500486e3260d5f6badaab0758f8
BosshuBots <ajmalitsme@gmail.com>
2026-03-22 07:24:23 +0530

    Update _init_.py

--- src/_init_.py
+++ src/_init_.py
@@ -1,26 +1,21 @@
 """
-Kinva Master - Complete Video & Image Editing Bot
+Kinva Master - Main Package
 Author: @kinva_master
-Website: https://kinva-master.com
 """
 
 __version__ = "1.0.0"
 __author__ = "Kinva Master"
 __license__ = "MIT"
 
-from .app import app
-from .bot import main as bot_main
 from .config import Config
 from .database import Database
-from .models import User, Design, Video, Payment
+from .models import User, Design, Video, Image
 
 __all__ = [
-    'app',
-    'bot_main',
     'Config',
     'Database',
     'User',
     'Design',
     'Video',
-    'Payment'
+    'Image'
 ]
